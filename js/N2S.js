61dcba722cda74e1b9e1e0bf98ab370ed9ae5ddc
vinson-han <han_vinson@student.smc.edu>
2023-01-11 18:39:04 -0800

    daily

---
+++ js/N2S.js
@@ -0,0 +1,18 @@
+function tribonacci(signature,n){
+  
+  if(n === 0)
+    return []
+
+  let arr = []
+	
+  for(let x = 0; x < n; x++){
+    if(signature[x] !== 'undefined' && x < 3){
+      arr.push(signature[x])
+    } else {
+      let y = arr[x-1] + arr[x-2] + arr[x-3]
+      arr.push(y)
+    }
+  }
+
+  return (arr)
+}
